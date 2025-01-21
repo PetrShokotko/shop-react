3d624a3cda0f727132c48fceb992c0121db645bd
PetrShokotko <shokotkop@gmail.com>
2025-01-21 11:35:49 +0200

    finish item

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,16 +1,47 @@
 
-import React, { Component } from 'react'
+import  { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Item extends Component {
-  static propTypes = {}
+  static propTypes = {
+    item: PropTypes.shape({
+      img: PropTypes.arrayOf(PropTypes.string).isRequired,
+      title: PropTypes.string.isRequired,
+      desc: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+    }).isRequired,
+  };
 
   render() {
     return (
-      <div>
-        {this.props.item.title}
+      <div className='item'>
+        <img src={this.props.item.img[0]} alt={this.props.item.title} />
+        <h2>{this.props.item.title}</h2>
+        <p>{this.props.item.desc}</p>
+        <p className='price'>{this.props.item.price}$</p>
+        <div className='add-to-cart'>+</div>
       </div>
-    )
+    );
   }
 }
 
-export default Item
+export default Item;
+// import React, { Component } from 'react'
+
+// export class Item extends Component {
+//   static propTypes = {}
+
+//   render() {
+//     return (
+//       <div className='item'>
+//         <img src={this.props.item.img[0]} alt={this.props.item.title} />
+//         <h2>{this.props.item.title}</h2>
+//         <p>{this.props.item.desc}</p>
+//         <p>{this.props.item.price}$</p>
+//         <div className='add-to-cart'>+</div>
+//       </div>
+//     )
+//   }
+// }
+
+// export default Item
--- src/components/items.jsx
+++ src/components/items.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import Item from "./item"; // Не забудьте импортировать компонент Item
+import PropTypes from 'prop-types';
+import Item from './item'
 
 const Items = (props) => {
     return (
-        <main className="com1"> {/* Исправлено на className вместо classname */}
+        <main className="com1">
             {props.items.map(el => (
                 <Item key={el.id} item={el} />
             ))}
@@ -11,4 +11,30 @@ const Items = (props) => {
     );
 };
 
+Items.propTypes = {
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            title: PropTypes.string.isRequired,
+            desc: PropTypes.string.isRequired,
+            img: PropTypes.arrayOf(PropTypes.string).isRequired,
+            price: PropTypes.number.isRequired,
+        })
+    ).isRequired,
+};
+
 export default Items;
+// import React from "react";
+// import Item from "./item"; // Не забудьте импортировать компонент Item
+
+// const Items = (props) => {
+//     return (
+//         <main className="com1"> {/* Исправлено на className вместо classname */}
+//             {props.items.map(el => (
+//                 <Item key={el.id} item={el} />
+//             ))}
+//         </main>
+//     );
+// };
+
+// export default Items;
